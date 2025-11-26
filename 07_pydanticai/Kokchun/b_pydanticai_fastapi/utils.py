2721dea43aa0e17733725f5641f22d5c751c99b2
Erik Unevik <erikunevik@gmail.com>
2025-11-26 19:41:12 +0100

    push

---
+++ 07_pydanticai/Kokchun/b_pydanticai_fastapi/utils.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import duckdb
+
+DATA_PATH = Path(__file__).parent / "data"
+
+
+def query_duckdb(sql_code: str, parameters=None):
+    with duckdb.connect(DATA_PATH / "movies.duckdb") as conn:
+        # if parameters:
+        cursor = conn.execute(sql_code, parameters)
+        # else:
+        #     cursor = conn.execute(sql_code)
+
+        lower = sql_code.strip().lower()
+        if lower.startswith(("select", "from", "desc", "pragma")):
+            return cursor.df()
+
